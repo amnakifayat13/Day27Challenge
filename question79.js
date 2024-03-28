c88c4f2dd21f99998069cc04d85e5eeb092969cf
amnakifayat13 <hayatullahamna@gmail.com>
2024-03-28 22:03:19 +0500

    Day 27 Challenge

---
+++ question79.js
@@ -0,0 +1,9 @@
+"use strict";
+/* Creating and Accessing Object Properties: Construct an object representing a car with properties for make,
+model, and year. Then, show how you can access the model property of the car.*/
+let car = {
+    make: "Honda",
+    model: "Honda Civic",
+    year: 2023,
+};
+console.log(car.model);
---
+++ question80.ts
@@ -0,0 +1,14 @@
+/* Updating Object Properties: Add a property named color to the existing car object, and then update the year
+ property to 2021. Show how to perform these operations.*/
+
+ let car1 = {
+    make: "Honda",
+    model: "Honda Civic",
+    year: 2023,
+    color: "Red"
+};
+
+car1.color = "blue",
+car1.year = 2024,
+console.log(car1);
+
